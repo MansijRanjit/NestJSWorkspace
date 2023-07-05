a34be81d978aa3a3220a5ec8ded7fd52fb3d13a2
Mansij Ranjit <mansijranjit@gmail.com>
2023-07-05 09:46:22 +0545

    cool

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,15 +1,19 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
 import { MessageService } from './messages.service';
+import { NotFoundException } from '@nestjs/common';
 
 @Controller('messages')
 export class MessagesController {
+  constructor(public messageService: MessageService) {}
+  //OR we can do this
+  /*
   messageService: MessageService;
 
-  constructor() {
-    this.messageService = new MessageService();
+  constructor(service: MessageService) {
+    this.messageService = service;
   }
-
+  */
   @Get()
   listMessages() {
     return this.messageService.findAll();
@@ -21,7 +25,11 @@ export class MessagesController {
   }
 
   @Get('/:id')
-  getMessages(@Param('id') id: string) {
-    return this.messageService.findOne(id);
+  async getMessages(@Param('id') id: string) {
+    const message = await this.messageService.findOne(id);
+    if (!message) {
+      throw new NotFoundException('message not found');
+    }
+    return message;
   }
 }
--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,12 +1,16 @@
 import { MessageRepository } from './message.repository';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export class MessageService {
-  messageRepo: MessageRepository;
+  constructor(public messageRepo: MessageRepository) {}
+  //OR we can do this
+  /* messageRepo: MessageRepository;
 
-  constructor() {
-    this.messageRepo = new MessageRepository();
+  constructor(repo: MessageRepository) {
+    this.messageRepo = repo;
   }
-
+*/
   findOne(id: string) {
     return this.messageRepo.findOne(id);
   }
